8ee9e19b53429eaa6547f55984a7c2ed82c7a302
jangroeneveld <groeneveld1992@gmail.com>
2017-03-01 19:59:35 +0100

    Cleaned repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app',
-  templateUrl: './app.component.html',
+	selector: 'app',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+	constructor(){
+		document.body.style.padding = "0";
+		document.body.style.margin = "0";
+		document.body.style.top = "0";
+		document.body.style.left = "0";
+	}
 }
